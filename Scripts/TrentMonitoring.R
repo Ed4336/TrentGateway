3924d26543e11751b7fdbd60e347c6c5a897d0c8
Ed <lgxed2@nottingham.ac.uk>
2025-02-13 11:41:02 +0000

    update to scripts

--- Scripts/TrentMonitoring.R
+++ Scripts/TrentMonitoring.R
@@ -4,6 +4,7 @@ library(tmap)
 library(leaflet)
 library(osmdata)
 library(tidyverse)
+library(RColorBrewer)
 
 #trent river plot----
 coords <- c(-2.23, 52.77, -0.64, 53.72)
@@ -15,11 +16,50 @@ trent <- opq(bbox = coords) %>%
 riverLines <- trent$osm_lines %>% 
   filter(name=='River Trent')
 
+#transform to BNG
+riverLines <- st_transform(riverLines, crs = 27700)
+
 ggplot()+
-  geom_sf(data = river_lines)
+  geom_sf(data = riverLines)
 
 #trent monitoring----
-##fish
+
+#access monitoring sites
+monitorSites <- read_csv('Data/FW_Fish_Sites_2024-11-14.csv')
+
+#transform to spatial object
+SFmonitorSites <- st_as_sf(monitorSites, coords = c("SITE_RANKED_EASTING", "SITE_RANKED_NORTHING"), crs = 27700)
+
+#visualise
+ggplot()+
+  geom_sf(data = SFmonitorSites)+
+  geom_sf(data = riverLines, color = 'blue')
+
+#not all sites are on the trent - buffer river and extract only sites that are on the trent
+
+#define a buffer for the river
+buffer <- 50
+
+#create a buffer around river
+riverLinesBuff <- st_buffer(riverLines, dist = buffer)
+
+#filter to keep only sites that are within the buffer zone
+SFmonitorSitesTrent <- st_filter(SFmonitorSites, riverLinesBuff)
+
+# Extract the Y-coordinate (Northing) from the geometry
+SFmonitorSitesTrent$Northing <- st_coordinates(SFmonitorSitesTrent)[, 2]
+
+# Reorder to plot north to south
+SFmonitorSitesTrent$SITE_NAME <- factor(SFmonitorSitesTrent$SITE_NAME,
+                                        levels = SFmonitorSitesTrent$SITE_NAME[order(-SFmonitorSitesTrent$Northing)])
+
+ggplot()+
+  geom_sf(data = riverLinesBuff, color = 'blue')+
+  geom_sf(data = SFmonitorSitesTrent, aes(color = SITE_NAME), size = 3, alpha = 1)+
+  geom_sf(data = SFmonitorSitesTrent, shape = 1, size = 3, alpha = 1, color = 'black') +   
+  scale_color_manual(values = brewer.pal((length(unique(SFmonitorSitesTrent$SITE_NAME))),"Set3"))
+
+##fish----
 
 bulkCounts <- read_csv('Data/FW_Fish_Bulk_Measurements_2024-11-14.csv')
 
